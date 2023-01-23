185a6367df19f37cd541b23e7793314ed716c274
ELgamer <65742116+ELgamerETS2@users.noreply.github.com>
2023-01-23 16:29:44 +0100

    Adding convert command.

--- src/main/java/me/bteuk/converterplugin/Converter.java
+++ src/main/java/me/bteuk/converterplugin/Converter.java
@@ -1,12 +1,22 @@
 package me.bteuk.converterplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Path;
 
 public class Converter implements CommandExecutor {
@@ -36,11 +46,68 @@ public class Converter implements CommandExecutor {
             }
         }
 
+        //Get world.
+        String worldName = instance.getConfig().getString("world");
+
+        if (worldName == null) {
+            sender.sendMessage("Set the world in config.");
+            return true;
+        }
+
+        World world = Bukkit.getWorld(instance.getConfig().getString("world"));
+
+        if (world == null) {
+            sender.sendMessage("The world " + worldName + " does not exist.");
+            return true;
+        }
+
         //Get datafolder.
         Path folder = Path.of(instance.getDataFolder().getAbsolutePath()).resolve("post-processing");
-        folder.
+        String[] files = new File(folder.toString()).list();
+
+        JSONParser parser = new JSONParser();
+
+        for (String file : files) {
+
+            try (Reader reader = new FileReader(folder + "/" + file)) {
+
+                //Get the array of json objects.
+                JSONArray jsonArray = (JSONArray) parser.parse(reader);
+
+                //Iterate through array.
+                for (Object object : jsonArray) {
+
+                    JSONObject jObject = (JSONObject) object;
+
+                    //Get the location of the block.
+                    Location l = new Location(world, (int) jObject.get("x"), (int) jObject.get("y"), (int) jObject.get("z"));
+
+                    //Get the details of the block.
+
+
+                }
+
+
+            } catch (IOException | ParseException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
 
 
         return false;
+    }
+
+    //Check if block has properties.
+    private boolean hasProperties(String block) {
+
+        switch (block) {
+
+            case
+
+        }
+
     }
 }
